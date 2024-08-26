d19fb57628d746bad2c091c68b6e24716135087d
YeiserBytes <yeiser192@gmail.com>
2024-08-25 20:55:04 -0400

    🎨 Improve input validation and error handling in bin2dec.py

--- Bin2Dec/bin2dec.py
+++ Bin2Dec/bin2dec.py
@@ -6,5 +6,17 @@ def bin2dec(bin: str) -> int:
 
 
 if __name__ == "__main__":
-    bin = input("Enter a binary number: ")
-    print(bin2dec(bin))
+    while True:
+        bin = input("Enter a binary number: ")
+
+        if len(bin) >= 8:
+            print("Binary number must be less than 8 bits")
+        else:
+            break
+
+    for i in range(len(bin)):
+        if bin[i] != "0" and bin[i] != "1":
+            print("Invalid binary number")
+            break
+
+    print(f"Result: {bin} = {bin2dec(bin)}")
